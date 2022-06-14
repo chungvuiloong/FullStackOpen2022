0e4ad80c6c6b51242c3460b92c305a913eaf5f46
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-06-14 19:12:10 +0300

    solved importing routes

--- part1/course-information/src/App.js
+++ part1/course-information/src/App.js
@@ -1,32 +1,30 @@
 import './App.css';
-// import Header from './Components/Header/Header';
-import Header from './Components/Header';
-// import Header from "./Header";
+import Header from './Components/Header/Header';
+import Total from './Components/Total/Total';
+import Content from './Components/Content/Content';
+
 
 const App = () => {
-  const course = 'Half Stack application development'
+  const course = 'Half Stack application development';
 
-  // const part1 = 'Fundamentals of React'
-  // const exercises1 = 10
-  // const part2 = 'Using props to pass data'
-  // const exercises2 = 7
-  // const part3 = 'State of a component'
-  // const exercises3 = 14
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+
+  const total = exercises1 + exercises2 + exercises3;
 
   return (
     <div>
       <Header course={course}/>
-
-      {/* <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p> */}
+      <Content part={part1} exercise={exercises1}/>
+      <Content part={part2} exercise={exercises2}/>
+      <Content part={part3} exercise={exercises3}/>
+      <Total total={total} />
     </div>
   )
 }

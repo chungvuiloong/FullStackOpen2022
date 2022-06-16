8174d2358b9ba51dca966ef248adceb8fc5b300f
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-06-16 14:23:04 +0300

    added if else statement

--- part1/unicafe/src/Components/Statistics/Statistics.js
+++ part1/unicafe/src/Components/Statistics/Statistics.js
@@ -10,12 +10,12 @@ const Statistics = ({ good, neutral, bad, total, average, positive }) => {
             <h2>statistics</h2>
             { (total === 0) ? <div>No feedback given</div>
                 :   <>
-                        <StatisticLine text="good" value={good} />
+                        <StatisticLine text="good" value={good}></StatisticLine>
                         <StatisticLine text="neutral" value={neutral} />
                         <StatisticLine text="bad" value={bad} />
                         <StatisticLine text="total" value={total} />
                         <StatisticLine text="average" value={average} />
-                        <StatisticLine text="positive" value={positive} />
+                        <StatisticLine text="positive%" value={positive} />
                     </>
             }
             

1d9553e0d95d003c5b5fade05b23ac0f11223f99
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-06-23 11:34:34 +0300

    parts2 phonebook

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]) 
+
+  const [newName, setNewName] = useState('a new person')
+
+  const changeHandler = (e) => {
+    e.preventDefault();
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setNewName(() => ({ [name]: value }))
+    // console.log(value);
+    console.log(newName);
+  }
+
+  const addPerson = (e) => {
+    e.preventDefault()
+    console.log('button clicked', e.target)
+    console.log(persons);
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      {/* Form */}
+      <form onSubmit={addPerson}>
+        <div>name:    
+          <input type="text" 
+            name="name"
+            value={persons.name}
+            onChange={changeHandler} />
+        </div>
+
+        <div>number:  
+          <input type="text" 
+            name="number"
+            value={persons.number}
+            onChange={changeHandler} />
+        </div>
+
+        <div><button type="submit" >add</button></div>
+      </form>
+
+      <h2>Numbers</h2>
+      <div>{persons.map((person)=> 
+        <div key={person.name}>{person.name} {person.number}
+        </div>)}
+      </div>
+    </div>
+  )
+}
+
+export default App

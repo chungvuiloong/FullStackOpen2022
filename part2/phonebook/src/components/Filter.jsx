c76debba86ef18e9cd44d7981f73d65a3d369bbc
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-12-17 18:09:04 +0200

    Added clear button.

--- part2/phonebook/src/components/Filter.jsx
+++ part2/phonebook/src/components/Filter.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Filter = ({searchNameHandler}) => {
-    return <p>Filter list with the name: <input onChange={searchNameHandler} /></p>
+    return (
+        <p>
+            <div>Filter list with the name: <input onChange={searchNameHandler} /><button>Clear</button></div>
+        </p>
+        )
 };
 
 export default Filter;

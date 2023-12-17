82cdc3f57d46a3fbc7c23ec95e87005042ff7c06
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-12-17 17:54:51 +0200

    Added services

---
+++ part2/notes/src/services/notes.jsx
@@ -0,0 +1,23 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/notes'
+
+const getAll = () => {
+    const request = axios.get(baseUrl)
+    return request.then(response => {
+      return response.data
+    })
+  }
+
+const create = newObject => {
+  return axios.post(baseUrl, newObject)
+}
+
+const update = (id, newObject) => {
+  return axios.put(`${baseUrl}/${id}`, newObject)
+}
+
+export default { 
+  getAll: getAll, 
+  create: create, 
+  update: update 
+}

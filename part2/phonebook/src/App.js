6725fec31ed3db6f22d3cc76e2630b117b136abe
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-12-18 16:43:11 +0200

    Imported personServices getAll

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Filter from './components/Filter'
 import Form from './components/Form'
 import Persons from './components/Persons'
+import personServices from './services/people'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+    const [persons, setPersons] = useState([]) 
+    const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState("")
+    const [searchName, setSearchName] = useState("")
 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState("")
-  const [searchName, setSearchName] = useState("")
+    useEffect(() => {
+        personServices
+        .getAll()
+        .then(person => {
+            setPersons(person)
+        })
+    }, [])
 
   const nameChangeHandler = (e) => {
     e.preventDefault();

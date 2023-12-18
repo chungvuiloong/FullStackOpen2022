7eb7200cbb102268348cf78718cfa2550bdc9db5
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-12-18 16:42:44 +0200

    Fixed Error

--- part2/phonebook/src/components/Filter.jsx
+++ part2/phonebook/src/components/Filter.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Filter = ({searchNameHandler}) => {
     return (
-        <p>
+        <>
             <div>Filter list with the name: <input onChange={searchNameHandler} /><button>Clear</button></div>
-        </p>
+        </>
         )
 };
 

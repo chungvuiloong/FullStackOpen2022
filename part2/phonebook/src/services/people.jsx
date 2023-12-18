5c3f2df32a117a0eefe729101cc7873c9e8c7f9c
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-12-18 17:28:27 +0200

    Changed functions into async type. Add create function.

--- part2/phonebook/src/services/people.jsx
+++ part2/phonebook/src/services/people.jsx
@@ -1,9 +1,14 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:3001/persons'
 
-const getAll = () => {
+async function getAll () {
     const request = axios.get(baseUrl)
     return request.then(response => response.data)
 }
 
-export default { getAll }
+async function createNewPerson (newPerson) {
+    const request = axios.post(baseUrl, newPerson)
+    return request.then(response => response.data)
+}
+
+export default { getAll, createNewPerson }

1d6526f1a3166850a2c02d1ede16fefddffa79bb
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-12-19 10:41:42 +0200

    Delete function works for database.

--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import personServices from '../services/people'
 
 const Persons = ({ filteredName }) => {
-    return  filteredName.map((person)=> <div key={person.id}>{person.name} {person.number}</div>)
+
+    const deletePerson = (id) => {
+        // e.preventDefault()
+        personServices
+            .deletePersonId(id)
+            // .then(p => {
+            //     setPersons(persons.concat(p))
+            //     setNewName("")
+            //     setNewNumber("")
+            //     }
+            // )
+    }
+
+    return  (
+        <>
+            {filteredName
+                .map((person) => 
+                <div key={person.id}>
+                    <span>{person.name} {person.number}</span>
+                    <button onClick={()=> deletePerson(person.id)}>Delete</button>
+                </div>
+            )}
+        </>
+    )
 };
 
 export default Persons;

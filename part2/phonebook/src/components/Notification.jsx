aa1d0bc9449ee5775ece956838180a27082b41b7
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-12-20 17:29:18 +0200

    Imported stylings from App

--- part2/phonebook/src/components/Notification.jsx
+++ part2/phonebook/src/components/Notification.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 
-const Notification = ({ message, style }) => {
+const Notification = ({ message, notificationType }) => {
+
+    const successful = {         
+        fontSize: '25px', 
+        position: 'absolute', 
+        top: 0, 
+        right: 0, 
+        border: '2px solid green', 
+        backgroundColor: 'lightgreen'  
+    }
+
+    const error = {         
+        fontSize: '25px', 
+        position: 'absolute', 
+        top: 0, 
+        right: 0, 
+        border: '2px solid red', 
+        backgroundColor: 'pink'  
+    }
+
+    const notificationStyle = notificationType === 'successful' ? successful : error
     return (
-        <div style={style}>
+        <div style={notificationStyle}>
             {message}
         </div>
     );

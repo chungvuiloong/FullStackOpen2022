82c7d901489732ab6d8393c540f3baa24f5d6da2
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-12-21 12:57:05 +0200

    Added search handler. Successfully mapped out data.

--- part2/dataforcountries/src/App.jsx
+++ part2/dataforcountries/src/App.jsx
@@ -1,19 +1,37 @@
 import { useEffect, useState } from 'react'
 import { getAllCountries } from './services/countryServices'
+import Search from './components/Search';
 
 function App() {
     const [countries, setCountries] = useState();
+    const [searchCountry, setSearchCountry] = useState();
 
     useEffect(() => {
         getAllCountries()
-        .then(countries => {
-        setCountries(countries)
+            .then(countriesData => {
+            setCountries(countriesData)
         })
-    }, [])
+    }, [])    
+    
+    const searchHandler = (e) => {
+        e.preventDefault();
+        const value = e.target.value;
+        setSearchCountry(value)
+        console.log(value);
+    }
+
 
   return (
     <>
-
+        <Search searchHandler={searchHandler} />
+        <div>
+            {
+                countries?.map((country, i) => 
+                    <div key={i}>
+                        {country.name.common}
+                    </div>
+            )}
+        </div>
     </>
   )
 }

24dccce7e4680b9f016b93f686511ce86a2047f4
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-12-23 15:53:16 +0200

    Added getCountryWeather

--- part2/dataforcountries/src/services/countryServices.jsx
+++ part2/dataforcountries/src/services/countryServices.jsx
@@ -1,9 +1,15 @@
 import axios from 'axios'
 const baseUrl = `https://studies.cs.helsinki.fi/restcountries/api`;
+const WEATHER_API_KEY = import.meta.env.VITE_WEATHER_API_KEY
 
 async function getAllCountries () {
     const request = axios.get(`${baseUrl}/all`)
     return request.then(response => response.data)
 }
 
-export { getAllCountries }
+async function getCountryWeather () {
+    const request = axios.get(`https://api.openweathermap.org/data/2.5/weather?q=georgia,europe&appid=${WEATHER_API_KEY}`)
+    return request.then(response => response.data)
+}
+
+export { getAllCountries, getCountryWeather }

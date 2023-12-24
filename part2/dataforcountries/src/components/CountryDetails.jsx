73af25bc509c3afce04e1cd09556ac40d12e7ea3
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-12-24 14:58:58 +0200

    Created a countryDetails component.

---
+++ part2/dataforcountries/src/components/CountryDetails.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+
+function countryDetails (country) {
+
+    return (         
+        <>
+            <div>{country?.name?.common}</div>
+            <div>Capital: {country?.capital}</div>
+            <div>Area: {country?.area}</div>
+            <div>
+                <div>Languages</div>
+                <ul>
+                    {
+                        Object.values(country?.languages).map((language, key) => (
+                            <li key={key}>
+                                {language}
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
+            <div>
+                <img src={Object.values(country?.flags)[0]} />
+            </div>
+        </>
+    )
+}
+
+export default countryDetails;

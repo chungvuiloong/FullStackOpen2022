b3bd3ad6b918bb1ebca254cb5ae9560239ba8923
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-12-25 14:53:29 +0200

    Added regec to put space after coma.

--- part2/dataforcountries/src/components/Weather.jsx
+++ part2/dataforcountries/src/components/Weather.jsx
@@ -1,10 +1,8 @@
-import React from 'react';
-
-const Weather = ({ countryWeatherInfo }) => {
+const Weather = ({ countryWeatherInfo}) => {
 
     return (
         <>
-            {/* <div>Weather in {capitalCountry}</div> */}
+            <div>Weather in {capitalCountry.replace(/,/g, ', ')}</div>
             <div>Temperature {Object.values(countryWeatherInfo?.main)[0] || {}} celsius</div>
             <img src={`https://openweathermap.org/img/wn/${Object.values(countryWeatherInfo?.weather[0])[3]}.png`}/>
             <div>Wind: {Object.values(countryWeatherInfo?.wind)[0]} m/s</div>

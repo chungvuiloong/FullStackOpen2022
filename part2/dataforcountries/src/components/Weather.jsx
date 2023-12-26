d10fb26a51fddab39129808d6a1a8334cb735fc6
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-12-26 10:01:50 +0200

    Fixed Weather component.

--- part2/dataforcountries/src/components/Weather.jsx
+++ part2/dataforcountries/src/components/Weather.jsx
@@ -1,8 +1,20 @@
-const Weather = ({ countryWeatherInfo}) => {
+import { useState, useEffect } from 'react';
+import { getCountryWeather } from '../services/countryServices'
+
+const Weather = ({ country }) => {
+    const [countryWeatherInfo, setCountryWeatherInfo] = useState()
+    if (country) {
+        useEffect(() => {
+            getCountryWeather(`${country.capital},${country.name.common}`)
+            .then(w => {
+                setCountryWeatherInfo(w)
+            })
+        }, [])    
+    }
 
     return (
         <>
-            <div>Weather in {capitalCountry.replace(/,/g, ', ')}</div>
+            <div>Weather in {country?.capital}</div>
             <div>Temperature {Object.values(countryWeatherInfo?.main)[0] || {}} celsius</div>
             <img src={`https://openweathermap.org/img/wn/${Object.values(countryWeatherInfo?.weather[0])[3]}.png`}/>
             <div>Wind: {Object.values(countryWeatherInfo?.wind)[0]} m/s</div>

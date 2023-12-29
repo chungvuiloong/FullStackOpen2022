218473c4e3d9c67b9d1681bf1721e03cf505de30
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-12-29 15:39:21 +0200

    Fixed bug by changing from js to cjs

--- part3/notes/index.cjs
+++ part3/notes/index.cjs
@@ -19,12 +19,37 @@ let notes = [
     }
   ]
 
+  app.use(express.json())
+
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
   response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+  
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+  response.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+  const note = request.body
+  console.log(note)
+  response.json(note)
 })
 
 const PORT = 3001

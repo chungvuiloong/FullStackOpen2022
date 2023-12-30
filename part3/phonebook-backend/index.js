ae33d60be60638f1ef005f88534d7a792ab3b839
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-12-30 22:12:01 +0200

    COMPLETE 3.4: Phonebook backend step4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -51,6 +51,14 @@ app.get('/api/persons/:id', (req, res) => {
     res.json(note)
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(p => 
+        p.id !== id 
+    )
+    response.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)

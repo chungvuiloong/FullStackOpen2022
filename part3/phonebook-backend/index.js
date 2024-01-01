68103745fe0919a46643d8a9ffc54c93856d2e07
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2024-01-01 14:59:56 +0200

    COMPLETED 3.7: Phonebook backend step7

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+var morgan = require('morgan')
 app.use(express.json())
 
 let persons = [
@@ -24,6 +25,8 @@ let persons = [
       "number": "39-23-6423122"
     }
 ]
+
+app.use(morgan('tiny'))
 
 app.get('/', (request, res) => {
     res.send('<h1>Phonebook backend</h1>')

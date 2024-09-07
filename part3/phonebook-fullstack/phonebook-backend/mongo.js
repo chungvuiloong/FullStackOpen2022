1bdc040ee8ae526ef819f3461d5110e8489b0417
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2024-09-07 21:21:01 +0300

    Fixed issue.

--- part3/phonebook-fullstack/phonebook-backend/mongo.js
+++ part3/phonebook-fullstack/phonebook-backend/mongo.js
@@ -7,7 +7,8 @@ if (process.argv.length<3) {
 
 const password = process.argv[2]
 const name = process.argv[3]
-const number = process.argv[3]
+const number = process.argv[4]
+console.log(name, number)
 const url =
   `mongodb+srv://fullstack:${password}@fullstack.uibsh.mongodb.net/?retryWrites=true&w=majority&appName=fullstack`
 

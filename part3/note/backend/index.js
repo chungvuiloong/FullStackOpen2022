b8c224f90e77c47925f6790eb5974357f464c59f
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2024-09-14 21:31:49 +0300

    Added update functionality.

--- part3/note/backend/index.js
+++ part3/note/backend/index.js
@@ -62,6 +62,21 @@ let notes = [
     .catch(error => next(error))
   })
 
+  app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+  
+    const note = {
+      content: body.content,
+      important: body.important,
+    }
+  
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+      .then(updatedNote => {
+        response.json(updatedNote)
+      })
+      .catch(error => next(error))
+  })
+
   const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
   }

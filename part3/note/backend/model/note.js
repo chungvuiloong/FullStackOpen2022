983ec16d4c416117e20bdb360460a6b77b91d519
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2024-09-15 11:39:32 +0300

    Deleted unnecessary.

--- part3/note/backend/model/note.js
+++ part3/note/backend/model/note.js
@@ -1,22 +1,4 @@
-// const config = require('./utils/config')
 const mongoose = require('mongoose')
-
-// mongoose.set('strictQuery', false)
-
-// const password = process.env.MONGODB_PASSWORD
-// const cluster = process.env.MONGODB_CLUSTER
-// const db = process.env.MONGODB_DB
-// const url =
-//   `mongodb+srv://fullstack:${password}@${cluster}.uibsh.mongodb.net/${db}?retryWrites=true&w=majority`
-// mongoose.set('strictQuery',false)
-// // mongoose.connect(config.MONGODB_URI)
-// mongoose.connect(url)
-//     .then(result => {
-//         console.log('connected to MongoDB')
-//     })
-//     .catch((error) => {
-//         console.log('error connecting to MongoDB:', error.message)
-//     })
 
 const noteSchema = new mongoose.Schema({
     content: String,

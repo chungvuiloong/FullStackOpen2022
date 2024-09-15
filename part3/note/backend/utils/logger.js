b255d6d79280bab4a0f4f59e5fd8fb252a6470c7
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2024-09-15 11:37:29 +0300

    Udpated notes.

--- part3/note/backend/utils/logger.js
+++ part3/note/backend/utils/logger.js
@@ -1,31 +1,11 @@
-const logger = require('./logger')
-
-const requestLogger = (request, response, next) => {
-    logger.info('Method:', request.method)
-    logger.info('Path:  ', request.path)
-    logger.info('Body:  ', request.body)
-    logger.info('---')
-    next()
+const info = (...params) => {
+    console.log(...params)
 }
 
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-}
-
-const errorHandler = (error, request, response, next) => {
-    logger.error(error.message)
-
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
-
-    next(error)
+const error = (...params) => {
+    console.error(...params)
 }
 
 module.exports = {
-    requestLogger,
-    unknownEndpoint,
-    errorHandler
+    info, error
 }

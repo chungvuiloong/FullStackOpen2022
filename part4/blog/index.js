7d721ddce1e8c8fff176b56aa22a2cd1d5fda9b0
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2024-09-16 10:07:36 +0300

    Used dummy data for blog.

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -1,8 +1,32 @@
 const http = require('http')
 
+let blogs = [
+    {
+      id: "1",
+      title: "Learning HTML",
+      author: "Jane Doe",
+      url: "http://example.com/html",
+      likes: 25
+    },
+    {
+      id: "2",
+      title: "Understanding JavaScript",
+      author: "John Smith",
+      url: "http://example.com/js",
+      likes: 45
+    },
+    {
+      id: "3",
+      title: "HTTP Methods: GET vs POST",
+      author: "Alice Johnson",
+      url: "http://example.com/http-methods",
+      likes: 60
+    }
+  ]
+  
 const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
+  response.writeHead(200, { 'Content-Type': 'application/json' })
+  response.end(JSON.stringify(blogs))
 })
 
 const PORT = 3001

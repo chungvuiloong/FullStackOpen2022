5d85b1a6fd6b55aa48edfb3d5e3cf01a1ed7f00a
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2024-09-16 10:10:53 +0300

    Installed express

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -2,33 +2,34 @@ const http = require('http')
 
 let blogs = [
     {
-      id: "1",
-      title: "Learning HTML",
-      author: "Jane Doe",
-      url: "http://example.com/html",
-      likes: 25
+        id: "1",
+        title: "Learning HTML",
+        author: "Jane Doe",
+        url: "http://example.com/html",
+        likes: 25
     },
     {
-      id: "2",
-      title: "Understanding JavaScript",
-      author: "John Smith",
-      url: "http://example.com/js",
-      likes: 45
+        id: "2",
+        title: "Understanding JavaScript",
+        author: "John Smith",
+        url: "http://example.com/js",
+        likes: 45
     },
     {
-      id: "3",
-      title: "HTTP Methods: GET vs POST",
-      author: "Alice Johnson",
-      url: "http://example.com/http-methods",
-      likes: 60
+        id: "3",
+        title: "HTTP Methods: GET vs POST",
+        author: "Alice Johnson",
+        url: "http://example.com/http-methods",
+        likes: 60
     }
   ]
   
 const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' })
-  response.end(JSON.stringify(blogs))
+    response.writeHead(200, { 'Content-Type': 'application/json' })
+    response.end(JSON.stringify(blogs))
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})

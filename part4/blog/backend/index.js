13c59cfdb101ee749bf2b171ba7171d30329c721
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2024-09-16 13:54:46 +0300

    Added minddleware

--- part4/blog/backend/index.js
+++ part4/blog/backend/index.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
-
 app.use(express.json())
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+  }
+  
 
 let blogs = [
 
@@ -52,6 +60,13 @@ let blogs = [
     response.json(blog)
   })
 
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+  
+app.use(unknownEndpoint)
+
+app.use(requestLogger)
 const PORT = 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)

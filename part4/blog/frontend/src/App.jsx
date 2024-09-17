b0b7c6845ed86ab35276b6e5d938604da1018cc6
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2024-09-17 09:41:16 +0300

    Added code to link frontend to backend.

--- part4/blog/frontend/src/App.jsx
+++ part4/blog/frontend/src/App.jsx
@@ -1,23 +1,42 @@
 import { useState, useEffect } from 'react'
+import axios from 'axios'
 import blogService from './services/blog'
 import './App.css'
 
 function App() {
   const [blogs, setBlogs] = useState([])
-//   const [newBlog, setNewBlog] = useState('')
+  const [newBlog, setNewBlog] = useState('')
 
   useEffect(() => {
     blogService.getAll()
       .then(response => {
-        console.log('promise fulfilled')
+        // console.log('promise fulfilled')
         setBlogs(response.data)
       })
   }, [blogs])
 
+  const add_blog = (event) => {
+    event.preventDefault()
+    const noteObject = {
+        name: "Author Test",
+        title: "title",
+        author: "author",
+        url: "url",
+        likes: 9999
+        }
+
+    axios
+        .post('http://localhost:3003/api/blogs', noteObject)
+        .then(response => {
+        setBlogs(blogs.concat(response.data))
+        setNewBlog('')
+        })
+  }
+
   return (
     <>
         <h1>Blog</h1>
-        <form >
+        <form onSubmit={add_blog}>
             <input
             // value={newBlog}
             />

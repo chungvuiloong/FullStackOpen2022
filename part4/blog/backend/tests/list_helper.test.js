02b5592a221b42704fa3e73c27399c97e80037ec
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2024-09-19 09:16:58 +0300

    COMPLETED 4.4: Helper Functions and Unit Tests, step 2

--- part4/blog/backend/tests/list_helper.test.js
+++ part4/blog/backend/tests/list_helper.test.js
@@ -1,4 +1,4 @@
-const { test } = require('node:test')
+const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
@@ -9,20 +9,48 @@ test('dummy returns one', () => {
     assert.strictEqual(result, 1)
 })
 
-// describe('total likes', () => {
-//     const listWithOneBlog = [
-//         {
-//             _id: '5a422aa71b54a676234d17f8',
-//             title: 'Go To Statement Considered Harmful',
-//             author: 'Edsger W. Dijkstra',
-//             url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-//             likes: 5,
-//             __v: 0
-//         }
-//     ]
+describe('total likes', () => {
+    const listWithNoBlog = [
 
-//     test('when list has only one blog, equals the likes of that', () => {
-//         const result = listHelper.totalLikes(listWithOneBlog)
-//         assert.strictEqual(result, 5)
-//     })
-// })
+    ]
+    const listWithOneBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 5,
+            __v: 0
+        }
+    ]
+    const listWithManyBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 5,
+            __v: 0
+        }
+    ]
+    test('of empty list is zero', () => {
+        const result = listHelper.totalLikes(listWithNoBlog)
+        assert.strictEqual(result, 0)
+    })
+    test('when list has only one blog, equals the likes of that', () => {
+        const result = listHelper.totalLikes(listWithOneBlog)
+        assert.strictEqual(result, 5)
+    })
+    test('of bigger list is calculated right', () => {
+        const result = listHelper.totalLikes(listWithManyBlog)
+        assert.strictEqual(result, 10)
+    })
+})
--- part4/blog/backend/utils/list_helper.js
+++ part4/blog/backend/utils/list_helper.js
@@ -1,7 +1,12 @@
 const dummy = (blogs) => {
     return blogs.length === 0 ? 1 : blogs.length 
 }
+
+const totalLikes = (blogs) => {
+    return blogs.reduce((sum, blog) => sum + blog.likes, 0);
+}
   
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }

9ede504e798faca113e6dca0fe8853ee4c034b77
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2025-07-08 17:34:41 +0300

    COMPLETED 5.17: Blog List End To End Testing, step 1

--- part5/blog/e2e-tests/tests/blog_app.spec.js
+++ part5/blog/e2e-tests/tests/blog_app.spec.js
@@ -11,4 +11,14 @@ describe('Blog app', () => {
     await expect(page.getByLabel('Password')).toBeVisible()
     await expect(page.getByRole('button', { name: 'Login' })).toBeVisible()
   })
+
+  test('user can login with valid credentials', async ({ page }) => {
+    await page.getByLabel('Username').fill('testuser')
+    await page.getByLabel('Password').fill('password123')
+    await page.getByRole('button', { name: 'Login' }).click()
+
+    await expect(page.getByText('testuser logged-in')).toBeVisible()
+    await expect(page.getByRole('button', { name: 'logout' })).toBeVisible()
+    await expect(page.getByText('create new blog')).toBeVisible()
+  })
 })

4dd0e690322bde288a5ec9062f1015f8d896d2d7
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2025-07-08 12:59:54 +0300

    Implemented simple login functionality.

--- part5/blog/frontend/src/App.jsx
+++ part5/blog/frontend/src/App.jsx
@@ -1,79 +1,64 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
-import blogService from './services/blogs'
-import loginService from './services/login'
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
-  const [user, setUser] = useState(null)
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+    const [blogs, setBlogs] = useState([
+        { id: 1, title: 'Example Blog 1', author: 'Author 1' },
+        { id: 2, title: 'Example Blog 2', author: 'Author 2' }
+    ])
+    const [user, setUser] = useState(null)
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
 
-  useEffect(() => {
-    if (user) {
-      blogService.getAll().then(blogs =>
-        setBlogs( blogs )
-      )
+    const handleLogin = async (event) => {
+        event.preventDefault()
+        // Dummy login logic
+        if (username && password) {
+            setUser({ name: username })
+            setUsername('')
+            setPassword('')
+        } else {
+            console.log('Wrong credentials')
+        }
     }
-  }, [user])
 
-  const handleLogin = async (event) => {
-    event.preventDefault()
-    
-    try {
-      const user = await loginService.login({
-        username, password,
-      })
-      
-      blogService.setToken(user.token)
-      setUser(user)
-      setUsername('')
-      setPassword('')
-    } catch (exception) {
-      console.log('Wrong credentials')
-    }
-  }
-
-  return (
-    <div>
-      <div>
-        <h2>Log in to application</h2>
-        <form onSubmit={handleLogin} aria-label="login form">
-          <div>
-            <label htmlFor="username">Username</label>
-            <input 
-              id="username" 
-              name="username" 
-              type="text" 
-              value={username}
-              onChange={({ target }) => setUsername(target.value)}
-              autoComplete="username" 
-            />
-          </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <input 
-              id="password" 
-              name="password" 
-              type="password" 
-              value={password}
-              onChange={({ target }) => setPassword(target.value)}
-              autoComplete="current-password" 
-            />
-          </div>
-          <button type="submit">Login</button>
-        </form>
-        {user && <p>{user.name} logged-in</p>}
-      </div>
-      
-      <div>
-        <h2>blogs</h2>
-        {blogs.map(blog =>
-          <Blog key={blog.id} blog={blog} />
-        )}
-      </div>
-    </div>
-  )
+    return (
+        <div>
+            <div>
+                <h2>Log in to application</h2>
+                <form onSubmit={handleLogin} aria-label="login form">
+                    <div>
+                        <label htmlFor="username">Username</label>
+                        <input
+                            id="username"
+                            name="username"
+                            type="text"
+                            value={username}
+                            onChange={({ target }) => setUsername(target.value)}
+                            autoComplete="username"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password</label>
+                        <input
+                            id="password"
+                            name="password"
+                            type="password"
+                            value={password}
+                            onChange={({ target }) => setPassword(target.value)}
+                            autoComplete="current-password"
+                        />
+                    </div>
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+            {user && <p>{user.name} logged-in</p>}
+            <h2>blogs</h2>
+            {user && blogs.map(blog =>
+                <Blog key={blog.id} blog={blog} />
+            )}
+        </div>
+    )
 }
 
 export default App
